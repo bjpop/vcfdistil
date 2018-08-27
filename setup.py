a376141d358b3da70406569de3b7c0c7260fa906
Bernie Pope <bjpope@unimelb.edu.au>
2018-08-27 22:37:55 +1000

    Basic iterators

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@
 from distutils.core import setup
 
 LONG_DESCRIPTION = \
-'''The program reads one or more input FASTA files. 
-For each file it computes a variety of statistics, and then
-prints a summary of the statistics as output.
-        
-The goal is to provide a solid foundation for new bioinformatics command line tools,
-and is an ideal starting place for new projects.'''
+'''The transforms and filters VCF files.'''
 
 
 setup(
@@ -23,7 +18,7 @@ setup(
     },
     url='https://github.com/bjpop/vcfdistil',
     license='LICENSE',
-    description=('A prototypical bioinformatics command line tool'),
+    description=('Transform and filter VCF files'),
     long_description=(LONG_DESCRIPTION),
-    install_requires=["biopython==1.66"],
+    install_requires=[],
 )
--- vcfdistil/vcfdistil.py
+++ vcfdistil/vcfdistil.py
@@ -6,24 +6,20 @@ License     : BSD-3-Clause
 Maintainer  : bjpope@unimelb.edu.au 
 Portability : POSIX
 
-The program reads one or more input FASTA files. For each file it computes a
-variety of statistics, and then prints a summary of the statistics as output.
 '''
 
 from argparse import ArgumentParser
-from math import floor
 import sys
 import logging
+from itertools import takewhile, tee, islice
+import re
 import pkg_resources
-from Bio import SeqIO
 
 
 EXIT_FILE_IO_ERROR = 1
 EXIT_COMMAND_LINE_ERROR = 2
-EXIT_FASTA_FILE_ERROR = 3
-DEFAULT_MIN_LEN = 0
+EXIT_VCF_FILE_ERROR = 3
 DEFAULT_VERBOSE = False
-HEADER = 'FILENAME\tNUMSEQ\tTOTAL\tMIN\tAVG\tMAX'
 PROGRAM_NAME = "vcfdistil"
 
 
@@ -52,15 +48,8 @@ def parse_args():
     Returns Options object with command line argument values as attributes.
     Will exit the program on a command line error.
     '''
-    description = 'Read one or more FASTA files, compute simple stats for each file'
+    description = 'Read one or more VCF files, compute simple stats for each file'
     parser = ArgumentParser(description=description)
-    parser.add_argument(
-        '--minlen',
-        metavar='N',
-        type=int,
-        default=DEFAULT_MIN_LEN,
-        help='Minimum length sequence to include in stats (default {})'.format(
-            DEFAULT_MIN_LEN))
     parser.add_argument('--version',
                         action='version',
                         version='%(prog)s ' + PROGRAM_VERSION)
@@ -68,137 +57,85 @@ def parse_args():
                         metavar='LOG_FILE',
                         type=str,
                         help='record program progress in LOG_FILE')
-    parser.add_argument('fasta_files',
+    parser.add_argument('vcf_files',
                         nargs='*',
-                        metavar='FASTA_FILE',
+                        metavar='VCF_FILE',
                         type=str,
-                        help='Input FASTA files')
+                        help='Input VCF files')
     return parser.parse_args()
 
 
-class FastaStats(object):
-    '''Compute various statistics for a FASTA file:
+metadata_regex = re.compile(r"\#\#(?P<key>[^=]*)=(?P<value>.*)$")
 
-    num_seqs: the number of sequences in the file satisfying the minimum
-       length requirement (minlen_threshold).
-    num_bases: the total length of all the counted sequences.
-    min_len: the minimum length of the counted sequences.
-    max_len: the maximum length of the counted sequences.
-    average: the average length of the counted sequences rounded down
-       to an integer.
-    '''
-    #pylint: disable=too-many-arguments
-    def __init__(self,
-                 num_seqs=None,
-                 num_bases=None,
-                 min_len=None,
-                 max_len=None,
-                 average=None):
-        "Build an empty FastaStats object"
-        self.num_seqs = num_seqs
-        self.num_bases = num_bases
-        self.min_len = min_len
-        self.max_len = max_len
-        self.average = average
 
-    def __eq__(self, other):
-        "Two FastaStats objects are equal iff their attributes are equal"
-        if type(other) is type(self):
-            return self.__dict__ == other.__dict__
-        return False
+def parse_metadata(metadata_dict, row):
+    match = re.match(metadata_regex, row)
+    if match is not None:
+        key = match.group('key')
+        value = match.group('value')
+        metadata_dict[key] = value
 
-    def __repr__(self):
-        "Generate a printable representation of a FastaStats object"
-        return "FastaStats(num_seqs={}, num_bases={}, min_len={}, max_len={}, " \
-            "average={})".format(
-                self.num_seqs, self.num_bases, self.min_len, self.max_len,
-                self.average)
 
-    def from_file(self, fasta_file, minlen_threshold=DEFAULT_MIN_LEN):
-        '''Compute a FastaStats object from an input FASTA file.
+def read_metadata(file):
+    metadata_dict = {}
+    num_metadata_rows = 0
+    for metadata_row in takewhile(lambda x: x.startswith('##'), file):
+        parse_metadata(metadata_dict, metadata_row)
+        num_metadata_rows += 1
+    return metadata_dict, num_metadata_rows
 
-        Arguments:
-           fasta_file: an open file object for the FASTA file
-           minlen_threshold: the minimum length sequence to consider in
-              computing the statistics. Sequences in the input FASTA file
-              which have a length less than this value are ignored and not
-              considered in the resulting statistics.
-        Result:
-           A FastaStats object
-        '''
-        num_seqs = num_bases = 0
-        min_len = max_len = None
-        for seq in SeqIO.parse(fasta_file, "fasta"):
-            this_len = len(seq)
-            if this_len >= minlen_threshold:
-                if num_seqs == 0:
-                    min_len = max_len = this_len
-                else:
-                    min_len = min(this_len, min_len)
-                    max_len = max(this_len, max_len)
-                num_seqs += 1
-                num_bases += this_len
-        if num_seqs > 0:
-            self.average = int(floor(float(num_bases) / num_seqs))
-        else:
-            self.average = None
-        self.num_seqs = num_seqs
-        self.num_bases = num_bases
-        self.min_len = min_len
-        self.max_len = max_len
-        return self
 
-    def pretty(self, filename):
-        '''Generate a pretty printable representation of a FastaStats object
-        suitable for output of the program. The output is a tab-delimited
-        string containing the filename of the input FASTA file followed by
-        the attributes of the object. If 0 sequences were read from the FASTA
-        file then num_seqs and num_bases are output as 0, and min_len, average
-        and max_len are output as a dash "-".
+mandatory_header_fields = ["#CHROM", "POS", "ID", "REF", "ALT", "QUAL", "FILTER", "INFO"]
 
-        Arguments:
-           filename: the name of the input FASTA file
-        Result:
-           A string suitable for pretty printed output
-        '''
-        if self.num_seqs > 0:
-            num_seqs = str(self.num_seqs)
-            num_bases = str(self.num_bases)
-            min_len = str(self.min_len)
-            average = str(self.average)
-            max_len = str(self.max_len)
-        else:
-            num_seqs = num_bases = "0"
-            min_len = average = max_len = "-"
-        return "\t".join([filename, num_seqs, num_bases, min_len, average,
-                          max_len])
+
+def parse_header(row):
+    '''returns the sample ids, if they are present'''
+    fields = row.split('\t')
+    num_mandatory = len(mandatory_header_fields)
+    print(fields[:num_mandatory])
+    if fields[:num_mandatory] == mandatory_header_fields:
+        remaining_fields = fields[num_mandatory:]
+        if len(remaining_fields) > 0 and remaining_fields[0] == "FORMAT":
+            sample_ids = remaining_fields[1:]
+            return sample_ids
+    else:
+        logging.warning("header missing mandatory fields: {}".format(row))
+    return None
+
+
+def read_header(file):
+    for header_row in takewhile(lambda x: x.startswith('#CHROM'), file):
+        return parse_header(header_row.strip())
+    return None
+
+
+def process_variants(file):
+    for row in file:
+        row = row.split()
+        print(row)
+
+
+def process_vcf_file(file):
+    metadata_iter, header_iter, variants_iter = tee(file, 3)
+    metadata, num_metadata_rows = read_metadata(metadata_iter)
+    print((metadata, num_metadata_rows))
+    sample_ids = read_header(islice(header_iter, num_metadata_rows, None, None))
+    print(sample_ids)
+    process_variants(islice(variants_iter, num_metadata_rows + 1, None, None))
 
 
 def process_files(options):
-    '''Compute and print FastaStats for each input FASTA file specified on the
-    command line. If no FASTA files are specified on the command line then
-    read from the standard input (stdin).
-
-    Arguments:
-       options: the command line options of the program
-    Result:
-       None
     '''
-    if options.fasta_files:
-        for fasta_filename in options.fasta_files:
-            logging.info("Processing FASTA file from %s", fasta_filename)
-            try:
-                fasta_file = open(fasta_filename)
-            except IOError as exception:
-                exit_with_error(str(exception), EXIT_FILE_IO_ERROR)
-            else:
-                with fasta_file:
-                    stats = FastaStats().from_file(fasta_file, options.minlen)
-                    print(stats.pretty(fasta_filename))
-    else:
-        logging.info("Processing FASTA file from stdin")
-        stats = FastaStats().from_file(sys.stdin, options.minlen)
-        print(stats.pretty("stdin"))
+    '''
+    for vcf_filename in options.vcf_files:
+        logging.info("Processing VCF file from %s", vcf_filename)
+        try:
+            vcf_file = open(vcf_filename)
+        except IOError as exception:
+            exit_with_error(str(exception), EXIT_FILE_IO_ERROR)
+        else:
+            with vcf_file:
+                process_vcf_file(vcf_file)
 
 
 def init_logging(log_filename):
@@ -227,7 +164,6 @@ def main():
     "Orchestrate the execution of the program"
     options = parse_args()
     init_logging(options.log)
-    print(HEADER)
     process_files(options)
 
 
